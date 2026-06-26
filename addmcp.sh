2a7af88fa5105c2055599b15582b057bc9524984
Angel Casado <acasado@acm.org>
2026-06-26 09:57:40 +0200

    sync

--- addmcp.sh
+++ addmcp.sh
@@ -1,1 +1,2 @@
- claude mcp add sgp --transport streamable-http http://localhost:9010/sgp/
+ #claude mcp add sgp --transport streamable-http http://localhost:9010/sgp/
+ claude mcp add sgp --transport streamable-http http://sgp.ngrok.dev/sgp/
